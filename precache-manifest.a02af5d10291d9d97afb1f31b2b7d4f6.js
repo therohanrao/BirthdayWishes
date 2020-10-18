a65be222e68db38b888954d4042eb477fc883ffb
therohanrao <59488758+therohanrao@users.noreply.github.com>
2020-10-17 22:17:07 -0700

    Updates

---
+++ precache-manifest.a02af5d10291d9d97afb1f31b2b7d4f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cd718fac718dad4d1455beea9591a6",
+    "url": "/BirthdayWishes/index.html"
+  },
+  {
+    "revision": "4207965b46bb4183cad6",
+    "url": "/BirthdayWishes/static/css/main.3c98a0b6.chunk.css"
+  },
+  {
+    "revision": "d73d6292fa237d98e89c",
+    "url": "/BirthdayWishes/static/js/2.044bb91c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/BirthdayWishes/static/js/2.044bb91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4207965b46bb4183cad6",
+    "url": "/BirthdayWishes/static/js/main.95823b08.chunk.js"
+  },
+  {
+    "revision": "56eeb84bdf750dfaf8b9",
+    "url": "/BirthdayWishes/static/js/runtime-main.6c242ba7.js"
+  },
+  {
+    "revision": "9b3faae292f48a816682ecca0915d40e",
+    "url": "/BirthdayWishes/static/media/cake.9b3faae2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BirthdayWishes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d9357f23807081e3822a326f3a9a284d",
+    "url": "/BirthdayWishes/static/media/sound.d9357f23.ogg"
+  }
+]);
